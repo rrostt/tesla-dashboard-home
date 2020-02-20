d4290d6772ab4f53a3665417e6f9915987b54faf
Mattias Rost <rost@mattiass-mbp.lan>
2020-02-20 21:04:10 +0100

    Remove controls ui in map

--- components/MyMap.js
+++ components/MyMap.js
@@ -5,7 +5,16 @@ import { GOOGLE_MAPS_KEY } from '../config'
 import {GoogleApiWrapper, Map, Marker} from 'google-maps-react'
 
 const MyMapContainer = ({google, lat, lon: lng, children, locations}) =>
-    <Map google={google} zoom={14} initialCenter={{lat, lng}}>
+    <Map google={google}
+        zoom={14}
+        style={{width: '100%', height: '100%', position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}}
+        center={{lat, lng}}
+        mapTypeControl={false}
+        scaleControl={false}
+        fullscreenControl={false}
+        streetViewControl={false}
+        zoomControl={false}
+    >
         <Marker
         name={'Car'}
         position={{lat, lng}} />
