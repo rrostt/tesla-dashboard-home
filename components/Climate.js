a93076f12d2ef001a9752d97776f323e53dfcd28
Mattias Rost <rost@mattiass-mbp.lan>
2020-02-20 21:33:23 +0100

    Increase clarity when climate on.

--- components/Climate.js
+++ components/Climate.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export const Climate = ({ inside, outside, switchingClimate, isOn, turnOn, turnOff }) =>
     <div style={{
-        color: switchingClimate ? '#f00' : isOn ? '#9797ff' : '#848383',
+        color: switchingClimate ? '#f00' : isOn ? '#009fff' : '#848383',
+        fontSize: isOn ? '125%' : '100%',
+        fontWeight: isOn ? 'bold' : 'normal',
         padding: 30
     }} onClick={() => switchingClimate || (isOn ? turnOff() : turnOn())}>
         {inside || '--'}&deg;C / {outside || '--'}&deg;C
