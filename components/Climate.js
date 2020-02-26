ae562e53daba43e1f9bf7e304975edd0262d2b89
Mattias Rost <rost@Mattiass-MacBook-Pro.local>
2020-02-26 13:38:55 +0100

    Add fire icon and busy icon to climate

--- components/Climate.js
+++ components/Climate.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { GiCampfire } from 'react-icons/gi'
+import { FaHourglassHalf } from 'react-icons/fa'
 
 export const Climate = ({ inside, outside, switchingClimate, isOn, turnOn, turnOff }) =>
     <div style={{
-        color: switchingClimate ? '#f00' : isOn ? '#009fff' : '#848383',
+        position: 'relative',
+        color: isOn ? '#009fff' : '#848383',
+        opacity: switchingClimate ? '30%' : '100%',
         fontSize: isOn ? '125%' : '100%',
         fontWeight: isOn ? 'bold' : 'normal',
         padding: 30
     }} onClick={() => switchingClimate || (isOn ? turnOff() : turnOn())}>
         {inside || '--'}&deg;C / {outside || '--'}&deg;C
+        <div style={{
+                position: 'absolute',
+                top: -70,
+                fontSize: 68,
+                left: 0,
+                right: 0,
+                width: '100%',
+                textAlign: 'center'
+        }}>
+            { (isOn && !switchingClimate) && <GiCampfire /> }
+            { switchingClimate && <FaHourglassHalf /> }
+        </div>
     </div>;
